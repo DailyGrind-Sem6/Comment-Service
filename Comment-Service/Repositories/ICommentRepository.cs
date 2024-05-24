3a8ceddb6e6759c475929bc57c74e1b62f82142a
Gökay <gokayatalay2@outlook.com>
2024-05-24 16:52:56 +0200

    FIll repositories

--- Comment-Service/Repositories/ICommentRepository.cs
+++ Comment-Service/Repositories/ICommentRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Comment_Service.Repositories;
+﻿using Comment_Service.Entities;
+
+namespace Comment_Service.Repositories;
 
 public interface ICommentRepository
 {
-    
+    public Task<List<Comment>> GetAll();
+    public Task<Comment> Get(string id);
+    public Task<Comment> Create(Comment comment);
+    public Task Update(string id, Comment commentIn);
+    public Task Remove(string id);
 }
